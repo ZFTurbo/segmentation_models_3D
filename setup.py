a3b3458f13b7e911c499b8cff1a9e1c5bdacfb1d
user-miner1 <amvnews@mail.ru>
2022-05-10 18:08:27 +0300

    Release v1.0.4 preparation

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
     install_requires=[
         'tensorflow>=2.8.0',
         "keras_applications>=1.0.8",
-        "classification_models_3D>=1.0.4",
+        "classification_models_3D>=1.0.6",
     ],
 )
